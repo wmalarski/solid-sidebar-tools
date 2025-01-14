b100b3aeaa57a2536d167e9af38828f1725ba4d1
Wojciech Malarski <w.malarski@gmail.com>
2025-01-14 22:21:16 +0100

    adds libraries setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createSignal } from "solid-js";
+import "./App.css";
+import viteLogo from "/vite.svg";
+import solidLogo from "./assets/solid.svg";
 
 function App() {
-  const [count, setCount] = createSignal(0)
+  const [count, setCount] = createSignal(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} class="logo" alt="Vite logo" />
         </a>
-        <a href="https://solidjs.com" target="_blank">
+        <a href="https://solidjs.com" target="_blank" rel="noreferrer">
           <img src={solidLogo} class="logo solid" alt="Solid logo" />
         </a>
       </div>
       <h1>Vite + Solid</h1>
       <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button type="button" onClick={() => setCount((count) => count + 1)}>
           count is {count()}
         </button>
         <p>
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and Solid logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
+import Sonda from "sonda/vite";
 import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
+import solidPlugin from "vite-plugin-solid";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [solid()],
-	server: {
-		port: 3000,
-	},
-	build: {
-		sourcemap: true,
-		target: "esnext",
-	},
+  plugins: [tsconfigPaths(), solidPlugin(), Sonda({ enabled: false })],
+  server: {
+    port: 3000,
+  },
+  build: {
+    sourcemap: true,
+    target: "esnext",
+  },
 });
