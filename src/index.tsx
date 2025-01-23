370f191f00c17f3ce91c4ca393bd300f9573665b
Wojciech Malarski <w.malarski@gmail.com>
2025-01-23 19:14:29 +0100

    fixes name problem

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-import { App } from "./app";
 import "./index.css";
+import { App } from "./root";
 
 const root = document.getElementById("root");
 
