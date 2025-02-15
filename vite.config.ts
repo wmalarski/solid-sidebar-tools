54f2d630a37a03cd6a8091b3ef04ba352128d326
Wojtek Malarski <w.malarski@gmail.com>
2025-02-15 21:19:13 +0100

    removes sourcemap option

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import Sonda from "sonda/vite";
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import zipPack from "vite-plugin-zip-pack";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { manifestPlugin } from "./src/build/manifest-plugin";
 
@@ -10,12 +11,12 @@ export default defineConfig({
     solidPlugin(),
     Sonda({ enabled: true, open: false }),
     manifestPlugin(),
+    zipPack({ outFileName: "solid-sidebar-tools.zip" }),
   ],
   server: {
     port: 3000,
   },
   build: {
-    sourcemap: true,
     target: "esnext",
     rollupOptions: {
       input: ["index.html", "popup.html", "src/service-worker.ts"],
