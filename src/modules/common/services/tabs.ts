d0573710fd07cd69c8991f19d8bbe0acabb50fda
Wojtek Malarski <w.malarski@gmail.com>
2025-03-31 21:38:03 +0200

    removes some tabs service functions

--- src/modules/common/services/tabs.ts
+++ src/modules/common/services/tabs.ts
@@ -1,5 +1,3 @@
-import { createResource } from "solid-js";
-
 export const reloadChromeTab = async () => {
   await chrome.tabs.reload();
 };
@@ -13,49 +11,4 @@ export const getCurrentUrl = async () => {
   const tabs = await chrome.tabs.query(args);
   const url = tabs[0]?.url;
   return url && getUrlOrigin(url);
-};
-
-export const getCurrentChromeTab = () => {
-  return chrome.tabs.getCurrent();
-};
-
-export const getCurrentChromeTabUrl = async () => {
-  const tab = await getCurrentChromeTab();
-  return tab?.url;
-};
-
-type OnUpdatedListener = Parameters<
-  typeof chrome.tabs.onUpdated.addListener
->[0];
-
-type OnActivatedListener = Parameters<
-  typeof chrome.tabs.onActivated.addListener
->[0];
-
-export const onCurrentUrlChange = (callback: (url: string) => void) => {
-  const onUpdatedListener: OnUpdatedListener = (_tabId, changeInfo, tab) => {
-    if (changeInfo.url && tab.active) {
-      callback(getUrlOrigin(changeInfo.url));
-    }
-  };
-
-  const onActivatedListener: OnActivatedListener = async (activeInfo) => {
-    const tab = await chrome.tabs.get(activeInfo.tabId);
-    if (tab.url && tab.active) {
-      callback(getUrlOrigin(tab.url));
-    }
-  };
-
-  chrome.tabs.onActivated.addListener(onActivatedListener);
-  chrome.tabs.onUpdated.addListener(onUpdatedListener);
-
-  createResource(async () => {
-    const url = await getCurrentChromeTabUrl();
-    url && callback(getUrlOrigin(url));
-  });
-
-  return () => {
-    chrome.tabs.onActivated.removeListener(onActivatedListener);
-    chrome.tabs.onUpdated.removeListener(onUpdatedListener);
-  };
 };
